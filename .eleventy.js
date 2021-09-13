2dbe43334993c71fb6646eb1d44f0a054d3a7ffb
Paul Tuohy <78020227+paultuohy@users.noreply.github.com>
2021-09-14 02:11:35 +1000

    commit

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css");
     eleventyConfig.addPassthroughCopy("./src/fonts");
     eleventyConfig.addWatchTarget("./src/css/");
+    eleventyConfig.addWatchTarget("./src/fonts/");
     return {
       dir: {
         input: "src",
